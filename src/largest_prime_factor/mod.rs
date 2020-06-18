fcaea2c42150ab06bbd54ce31f9b72e92bbdedb4
Chakraborty <Sandeepan.Chakraborty@Honeywell.com>
2020-06-18 10:15:35 +0530

    completing lpf

--- src/largest_prime_factor/mod.rs
+++ src/largest_prime_factor/mod.rs
@@ -1,29 +1,33 @@
 use math;
 
 pub fn runner() {
-    let a = [10, 20, 30, 37, 50, 7, 99, 1007];
+    let a: [u64; 2] = [13195, 600851475143];
 
     for element in a.iter() {
         println!(
-            "{} is {}",
+            "lpf of {} is {}",
             *element,
-            if is_prime(*element) == true {
-                "prime"
-            } else {
-                "not prime"
-            }
+            calculate_lpf(*element)
         );
     }
 }
 
-pub fn lpf(n: u32) -> u32 {
-    
-    1
+pub fn calculate_lpf(n: u64) -> u64 {
+    let root = (n as f64).sqrt();
+    let root = math::round::ceil(root, 0) as u64;
+    let mut counter = root;
+    while counter >= 2 {
+        if n % counter == 0 && is_prime(counter) == true {
+            break;
+        }
+        counter -= 1;
+    }
+    return counter;
 }
 
-pub fn is_prime(n: u32) -> bool {
+pub fn is_prime(n: u64) -> bool {
     let root = (n as f64).sqrt();
-    let root = math::round::floor(root, 0) as u32;
+    let root = math::round::floor(root, 0) as u64;
     let mut counter = 2;
     let mut flag = true;
     while counter <= root {
